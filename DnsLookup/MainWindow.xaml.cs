4d0c77e155eebd72dec87a5572992bb386046c93
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-11-11 15:11:54 +0100

    Add project files.

---
+++ DnsLookup/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DnsLookup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string selectedDomainName = "a";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected UserSettings UserSettings { get; set; } = new();
+        protected IEnumerable<string> DomainNames => UserSettings?.DomainNames;
+        
+        // TODO move to viewmodel
+        public string SelectedDomainName
+        {
+            get => selectedDomainName; set
+            {
+                selectedDomainName = value;
+                OnPropertyChanged(nameof(SelectedDomainName));
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                UserSettings = await UserSettings.LoadAsync();
+            });
+        }
+
+        private void btnGetIp_Click(object sender, RoutedEventArgs e)
+        {
+            string domainName = txtDomainName.Text;
+            if (!string.IsNullOrWhiteSpace(domainName))
+            {
+                try
+                {
+                    var hostEntry = Dns.GetHostEntry(domainName);
+                    if (hostEntry is null || !hostEntry.AddressList.Any())
+                        return;
+
+                    var ipAddress = hostEntry.AddressList[0].MapToIPv4().ToString();
+                    txtIpAddress.Text = ipAddress;
+
+                    if (!UserSettings.DomainNames.Contains(domainName))
+                    {
+                        UserSettings.DomainNames.Add(domainName);
+                        Task.Run(async () => await UserSettings.SaveAsync());
+                    }
+
+                    if (UserSettings.CopyToClipboard)
+                        Clipboard.SetData(DataFormats.Text, ipAddress);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void checkCopyToClipboard_Checked(object sender, RoutedEventArgs e)
+        {
+            UserSettings.CopyToClipboard = checkCopyToClipboard.IsChecked ?? false;
+        }
+    }
+}
---
+++ DnsLookup/UserSettings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DnsLookup
+{
+    public class UserSettings
+    {
+        private List<string> domainNames = new();
+
+        public List<string> DomainNames
+        {
+            get => domainNames; set
+            {
+                domainNames = value;
+            }
+        }
+
+        public bool CopyToClipboard { get; set; }
+
+        [JsonIgnore]
+        public static string SettingsFilePath => Path.Join(AppDataDir, SettingsFileName);
+
+        [JsonIgnore]
+        public static readonly string SettingsFileName = "appsettings.json";
+
+        [JsonIgnore]
+        public static string AppDataDir => Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), nameof(DnsLookup));
+
+        public async static Task<UserSettings> LoadAsync()
+        {
+            if (!File.Exists(SettingsFilePath))
+            {
+                UserSettings initialSettings = new();
+                await initialSettings.SaveAsync();
+            }
+
+            using var fs = File.OpenRead(SettingsFilePath);
+            var settings = await JsonSerializer.DeserializeAsync<UserSettings>(fs);
+
+            return settings;
+        }
+
+        public async Task SaveAsync()
+        {
+            if (!File.Exists(AppDataDir))
+                Directory.CreateDirectory(AppDataDir);
+
+            string json = JsonSerializer.Serialize(this);
+            await File.WriteAllTextAsync(SettingsFilePath, json);
+        }
+    }
+}
